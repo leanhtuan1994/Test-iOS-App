4c793a3805de0f55ad38c8a2f8f44ec2e5059b00
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2016-07-16 00:22:21 +0700

    init commit

---
+++ Test iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test iOS App
+//
+//  Created by TUAN ANH LE on 7/15/16.
+//  Copyright © 2016 tuananhle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
